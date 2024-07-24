99d3be1f579f4efdb6d22b198df82e43757ef363
cookingnoob <costealan@gmail.com>
2024-07-24 13:28:35 -0600

    add try except block if the file was not found

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 def main(): 
     path = 'books/frankenstein.txt'
     text = get_book_text(path)
-    print(text)
+    counter = count_words(text)
+    print(counter)
 
-    
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+    try:
+        with open(path) as f:
+            return f.read()
+    except FileNotFoundError:
+        print(f'Error: the file at {path} was not found')
+        return ""
 
 main()
