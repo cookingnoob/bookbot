b791a9f4dbca79ea2ca936bba8a4961cf8a630ca
cookingnoob <costealan@gmail.com>
2024-07-24 13:21:27 -0600

    refactor main function. There is one function that handles opening the text file and other passing the path to the file"

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 def main(): 
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-        print(file_contents)
+    path = 'books/frankenstein.txt'
+    text = get_book_text(path)
+    print(text)
+
+    
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 main()
