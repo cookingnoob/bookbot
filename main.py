0b10b7e01b7577f89248f1b2ef79c5628068baa2
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2024-11-08 17:24:22 -0600

    add function to convert dictionary into list of dictionaries
    add printing list

--- main.py
+++ main.py
@@ -3,15 +3,30 @@ def main():
         book_path = 'books/frankenstein.txt'
         text = read_file(book_path)
         word_count = total_word_count(text)
-        print(f"{word_count} words found in the document")
         letters = separate_into_letters(text)
-        print(letters)
+        list_of_letters = convert_dictionary_to_list(letters)
+        print('--- Begin report of books/frankenstein.txt ---')
+        print(f"{word_count} words found in the document\n")
+        for letter in list_of_letters:
+            print(f"The '{letter['char']}' character was found {letter['num']} times")
+        print("--- End report ---")
+
 
     except FileNotFoundError:
         print('file was not found')
 
+def sort_on(dict):
+    return dict['num']
+
+def convert_dictionary_to_list(letters):
+    list_of_letters = []
+    for letter in letters:
+        list_of_letters.append({'char' : f'{letter}', 'num': letters[letter]})
+    list_of_letters.sort(reverse=True, key=sort_on)
+    return list_of_letters
+
 def separate_into_letters(text):
-    lower_case = all_letters_lowercase(text)
+    lower_case = text.lower()
     letter_dictionary = {}
     for word in lower_case:
         for letter in word:
@@ -19,17 +34,12 @@ def separate_into_letters(text):
                 letter_dictionary[letter] += 1
             elif letter.isalpha():
                 letter_dictionary[letter] = 1
-    return sorted(letter_dictionary.items(), key=lambda x:x[1], reverse=True)
-
-def all_letters_lowercase(text):
-    return text.lower()
+    return letter_dictionary
 
 def total_word_count(text):
-    words = separate_into_words(text)
+    words = text.split()
     return len(words)
 
-def separate_into_words(text):
-    return text.split()
 
 def read_file(book_path):
     with open(book_path) as f:
